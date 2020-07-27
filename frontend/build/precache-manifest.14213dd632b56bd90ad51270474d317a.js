11d7b14a698b2cdfcf3a9b7643144bf174c583a3
Dion <diontugit@gmail.com>
2020-07-27 16:22:57 -0400

    made build folder visible

---
+++ frontend/build/precache-manifest.14213dd632b56bd90ad51270474d317a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1906e8901a93e4e0dcb1ec91d2134948",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6da01c6f88c157e739f7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4d43d740477c0efb22b0",
+    "url": "/static/js/2.18c688ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.18c688ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6da01c6f88c157e739f7",
+    "url": "/static/js/main.0619bffa.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
