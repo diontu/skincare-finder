c4f8cdba547a9ef2104e6f1e913ab7b48f920548
Dion <diontugit@gmail.com>
2020-07-27 15:36:59 -0400

    added path for deployment

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const productsRouter = require('./api/routes/products.route');
+const path = require('path');
 
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
 app.use(cors());
 app.use(express.json()); // parses JSON requests and places them in request.body
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
 
 // connect with the DB
 const uri = process.env.DB_URI;
